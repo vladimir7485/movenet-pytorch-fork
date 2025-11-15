60453e3f62f83fda8622dfd53a4452c9cf04381f
Vladimir K. <fomcyou@gmail.com>
2025-11-15 11:50:19 +0300

    Added sh

---
+++ video_demo_3d.sh
@@ -0,0 +1,5 @@
+python video_demo_3d.py --model movenet_lightning \
+                --video_path \
+                ./videos/video_2025-11-15_01-38-21.mp4 \
+                --output_path \
+                ./videos/video_2025-11-15_01-38-21_pose2D.mp4
